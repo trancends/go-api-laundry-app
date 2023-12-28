bb0da1dfaa3277e38e6dcaeacbf9f87486ec3235
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2023-12-28 13:31:29 +0700

    init challenge go api

---
+++ config/database-connection.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "enigma"
+	password = "1234"
+	dbName   = "enigma_laundry_api"
+)
+
+func ConnectDB() *sql.DB {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", host, port, user, password, dbName)
+
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Coonected to Database")
+	return db
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module challenge-goapi
+
+go 1.21.5
+
+require github.com/lib/pq v1.10.9
