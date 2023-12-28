3e457ca2e0181ed4fb9426159060e8db8f9069fc
Benedictus Jullian Pradana <benedictusjullian1@gmail.com>
2023-12-28 15:09:00 +0700

    import gin create default route,create customer model

---
+++ models/customer.go
@@ -0,0 +1,8 @@
+package models
+
+type Customer struct {
+	Id          string `json:"id"`
+	Name        string `json:"name"`
+	PhoneNumber string `json:"phoneNumber"`
+	Address     string `json:"phoneNumber"`
+}
